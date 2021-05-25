707f2c1dd5563f7625113a89b076315245a53d12
Elias Eskelinen <elias.eskelinen@pm.me>
2021-05-25 15:09:42 +0300

    css cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,7 @@
 	<p class="credit">Hallacoin 3D-model made by @Anatoli</p>
 	<h1> Hallacoin </h1>
 	<p class="laudatur">Since 1940 - Confido Autem In Halla - Inventum Est Ksyk</p>
-    <div class="ohjeet">
+    <div class="ohje ohje-pariton">
 		<!-- <button on:click={go}> Lataa nyt </button> -->
 		<h2>Asennus Windowsille (64-bit)</h2>
 		<ol>
@@ -25,18 +25,17 @@
 		<li>Olet valmis käyttämään Hallacoinia!</li>
 		</ol>
 	</div>
-	<div class="mining">
+	<div class="ohje">
 		<h2>Kaivostoiminta</h2>
 		<ol>
 		<li>Lataa <a href="https://sourceforge.net/projects/cpuminer/files/latest/download">cpuminer</a></li>
 		<li>Mene Hallacoinin asennuskansioon ja suorita seuraava komento: Hallacoin-qt.exe -server -addresstype=legacy</li>
 		<li>Mene kansioon, johon latasit cpuminerin ja suorita seuraava komento: minerd -o http://localhost:9332/ --user darius --pass rucker --coinbase-addr=sun Hallacoin-osoite (Hallacoin osoite lyötyy vastaanota rahoja-osiosta)</li>
 	</div>
-	<div class="abouthalla">
+	<div class="ohje ohje-pariton">
 		<h2>Mikä on Hallacoin?</h2>
-		<p>Hallacoin on <a href="https://discord.gg/4HFVy2BAbe">Hallabois</a> palvelimen ylläpitämä kryptovaluutta.</p>
+		<p>Hallacoin on <a href="https://discord.gg/4HFVy2BAbe">Hallabois</a>-palvelimen ylläpitämä kryptovaluutta.</p>
 	</div>
-	
 </main>
 
 <style>
@@ -47,40 +46,26 @@
 	margin: 0;
 	padding: 0;
 }
-.credit{
-	text-align: middle;
-	font-size: .75em;
-	color: #aaaaaa;
-	margin-top: 0;
-	margin-bottom: 15px;
-}
-.ohjeet{
-	margin-top: 50px;
+
+/* Helpompi muuttaa kaikkia asioita kerralla  */
+.ohje {
+	padding-top: 50px;
 	padding-bottom: 50px;
+	margin: 0;
+	color: #EEEEEE;
+}
+.ohje-pariton {
 	background: #1e2f3e;
-	color: #EEEEEE;
-}
-.mining{
-	margin-top: 50px;
-	padding-bottom: 50px;
-	background: #1f2125;
-	color: #EEEEEE;
-}
-.abouthalla{
-	margin-top: 50px;
-	padding-bottom: 50px;
-	background: #1e2f3e;
-	color: #EEEEEE;
 }
 
+/*                                    */
 h1{
 	margin-top: 0;
 	margin-bottom: 0;
 	font-size: 400%;
 }
 h2{
-	padding-top: 30px;
-	margin-bottom: 20px;
+	margin: 0;
 }
 ol{
 	width: 50%;
