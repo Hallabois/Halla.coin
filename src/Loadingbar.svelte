0bedf5a2df8ca97ebc146031679ed363cf2a6584
Elias Eskelinen <elias.eskelinen@pm.me>
2021-05-25 14:49:28 +0300

    replaced the loading bar with text

--- src/Loadingbar.svelte
+++ src/Loadingbar.svelte
@@ -2,14 +2,12 @@
 	import { writable } from 'svelte/store';
 	export let loaded = false;
 	export let loaded_percent = 0;
-	let progress = writable(0);
-	$: progress.set(loaded_percent*100);
 </script>
 
 {#if !loaded}
 <main>
 	<p> Loading... </p>
-	<progress max="100" value={$progress}></progress>
+	<h3> {Math.round(loaded_percent * 100) / 100}% </h3>
 </main>
 {/if}
 
