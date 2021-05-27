0ef86a2e62a60f77ad39fe66f944e6145f3cdddb
Elias Eskelinen <elias.eskelinen@pm.me>
2021-05-27 13:22:52 +0300

    noni

--- src/App.svelte
+++ src/App.svelte
@@ -40,11 +40,12 @@
 		<ol>
 		<li>{$_('app.kaivaminen_win_1')} <a href="https://sourceforge.net/projects/cpuminer/files/latest/download">cpuminer</a></li>
 		<li>{$_('app.kaivaminen_win_2')} Hallacoin-qt.exe -server -addresstype=legacy</li>
-		<li>{$_('app.kaivaminen_win_3')} minerd -o http://localhost:9332/ --user darius --pass rucker --coinbase-addr=sun hallacoin-osoite (Hallacoin osoite löytyy vastaanota varoja-osiosta)</li>	</div>
+		<li>{$_('app.kaivaminen_win_3')} minerd -o http://localhost:9332/ --user darius --pass rucker --coinbase-addr=sun hallacoin-osoite (Hallacoin osoite löytyy vastaanota varoja-osiosta)</li>
 		</ol>
 		<br />
 		<br />
 		<a class="ohjelinkki" href="https://hallabois.github.io/Hallacoin-ohjeet/mining">Yksityiskohtaisempi ohje kaivostoimintaan</a>
+	</div>
 	<div class="ohje ohje-pariton">
 		<img src="hallacoin50000.png" alt="HallaCoin" width="80" height="80">
 		<h2>{$_('app.about_title')}</h2>
