0fb23bd13bee61ac89773bfadb28528d3a1347b7
BongCloudMain <eero.ristolainen1@gmail.com>
2021-06-03 11:49:38 +0300

    suurennettu kolikkoa

--- src/scene.svelte
+++ src/scene.svelte
@@ -16,7 +16,7 @@
 
 
 	const scene = new THREE.Scene();
-	const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+	const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
 	const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 	let renderer;
 
